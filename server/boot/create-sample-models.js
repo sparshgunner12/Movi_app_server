bea03fd3a3386bb01d673d8ae45a743296265814
Arsenal <sparshgunner12@gmail.com>
2015-05-15 04:20:59 +0530

    First Commit

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,17 @@
+module.exports = function(app) {
+  app.dataSources.msqlDs.automigrate('User', function(err) {
+    if (err) throw err;
+ 
+    app.models.User.create([
+      {email: 'sp@a.com', password: 'sp'},
+      {email: 'sparsh@iitg.ernet.in', password: 'sparsh'},
+      {email: 'arpit@iitg.ernet.in', password: 'arpit'},
+      {email: 'amit@iitg.ernet.in', password: 'amit'},
+      {email: 'shashank@iitg.ernet.in', password: 'shashank'},
+    ], function(err, coffeeShops) {
+      if (err) throw err;
+ 
+      console.log('Models created: \n', coffeeShops);
+    });
+  });
+};
